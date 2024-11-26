0a5b8f1825905b3208b0767091cf168a75d8f7d4
Kan_green <ksl2001@dgsw.hs.kr>
2024-11-26 14:18:59 +0900

    login
    login

--- dev_community/src/main/java/com/example/dev_community/controller/user/UserApiController.java
+++ dev_community/src/main/java/com/example/dev_community/controller/user/UserApiController.java
@@ -12,7 +12,7 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @PostMapping("/user")
+    @PostMapping("/login")
     public String signup(AddUserRequest request){
         userService.save(request);
         return "redirect:/login";
